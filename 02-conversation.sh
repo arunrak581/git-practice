c113f4e20e479fb2b8c60235fac71f61b816869d
arun <arun.rak581@gmail.com>
2024-08-26 22:54:02 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, How are you?"
+echo "suresh:: Hello Ramesh. I am fine.How are you doing?"
+echo "Ramesh:: I am doing good suresh.What's going on?"
+echo "Suresh:: I started learning shell script Ramesh"
