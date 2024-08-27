cc4f6f7d8c4ffe65f64546d9417b87bd048881e6
arun <arun.rak581@gmail.com>
2024-08-27 20:42:57 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi ${PERSON2},How are you?"
+echo "S${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "R$PERSON1:: I am doing good ${PERSON2}. What's going on?"
+echo "S${PERSON2}:: I started learning shell script $PERSON1"
