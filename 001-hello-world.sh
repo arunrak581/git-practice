058174d43aead66e867c17a74405c99680a5417c
arun <arun.rak581@gmail.com>
2024-08-27 18:06:20 +0530

    I created a new hello world

---
+++ 001-hello-world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#ctrl+s --> save,this is only info purpose.not part of code
+#author - arun kumar
+#Team - Devops
+echo "Hello world"
--- 01-hello-world.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#ctrl+s --> save,this is only info purpose.not part of code
-#author - arun kumar
-#Team - Devops
-echo "Hello world"
