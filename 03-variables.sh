7770cf5c671bf68c8d8c22ef02cf6b40d4460042
arunrak581 <arun.rak581@gmail.com>
2024-08-27 20:02:48 +0530

    Update 03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PERSON1=RAMESH
+PERSON1=RAHEEM
 PERSON2=SURESH
 echo "$PERSON1:: hi ${PERSON2},How are you?"
-echo "S${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
-echo "R$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "S${PERSON2}:: I started learning shell script $PERSON1"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning shell script $PERSON1"
