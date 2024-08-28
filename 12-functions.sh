fc82692e0b2c6e14baf959dcd366e7feeb5eabf0
arun <arun.rak581@gmail.com>
2024-08-29 00:35:35 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#echo "User Id is: $USERID"
+
+CHECK_ROOT() {
+ 
+     if [ $USERID -ne 0 ]
+     then
+         echo "Please run this scripts with root priveleges"
+         exit 1
+     fi
+}
 
 VALIDATE() {
     if [ $1 -ne 0 ]
@@ -13,11 +21,7 @@ VALIDATE() {
     fi         
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this scripts with root priveleges"
-    exit 1
-fi
+CHECK_ROOT
 
 dnf list installed git
 
