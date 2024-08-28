181d9a9c10b4435e7b0e41fce5ba7829c529042d
arun <arun.rak581@gmail.com>
2024-08-29 00:56:26 +0530

    functions

--- 13-colours.sh
+++ 13-colours.sh
@@ -21,7 +21,7 @@ VALIDATE(){
         echo -e "$2 is... $R FAILED $N"
         exit 1
     else
-        echo -e "$2 is.... SUCCESS $N"
+        echo -e "$2 is.... $G SUCCESS $N"
     fi         
 }
 
