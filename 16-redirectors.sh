2400707eed015c595adfaa1feb8ae3827b542959
arun <arun.rak581@gmail.com>
2024-09-06 17:40:58 +0530

    functions

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,7 +15,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this scripts with root priveleges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -35,7 +35,7 @@ USAGE(){
     exit 1
 }
 
-echo "Script started excuting at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
@@ -46,14 +46,13 @@ fi
 
 for package in $@ # $@ refers to all arguments passed to it
 do
-    
-    dnf list inastalled $package &>>$LOG_FILE
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed,going to install it.." | tee -a $LOG_FILE
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $?"InstallingG $package"
+        VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed,noting to do $N" | tee -a $LOG_FILE
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOG_FILE
     fi    
 done
